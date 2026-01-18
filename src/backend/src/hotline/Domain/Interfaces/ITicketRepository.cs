11d02e19dc1495969db43ac0706b3b79fe8de97c
Giba <giba.darocha@gmail.com>
2026-01-18 16:31:13 -0300

    Added First Hotline Implementation

---
+++ src/backend/src/hotline/Domain/Interfaces/ITicketRepository.cs
@@ -0,0 +1,12 @@
+﻿using Hotline.Domain.Entities;
+
+namespace Hotline.Domain.Interfaces;
+
+public interface ITicketRepository
+{
+    Task<IEnumerable<Ticket>> GetAllTicketsAsync();
+    Task<Guid> AddTicketAsync(Ticket ticket);
+    Task UpdateTicketAsync(Ticket ticket);
+    Task DeleteTicketAsync(int id);
+
+}
