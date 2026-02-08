07f832dcf9eceae05aa5a0d56e3acd0c5fa56f97
Gilberto <giba.darocha@gmail.com>
2026-02-08 16:19:28 -0300

    WIP - Adding to K3s Cluster

--- src/backend/eng/K8s/K3d.sh
+++ src/backend/eng/K8s/K3d.sh
@@ -4,3 +4,14 @@ k3d cluster create mentory -p "8081:80@loadbalancer" --agents 2
 # configure the kubectl to the new cluster
 k3d kubeconfig merge mentory --kubeconfig-switch-context
 
+# add backend webapi to cluster
+k3d image import hotline-webapi:latest -c mentory
+
+
+# apply the deploy and service
+kubectl apply -f deployment.yml
+kubectl apply -f service.yml
+
+
+# list of services
+kubectl get svc
