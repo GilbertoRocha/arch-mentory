c6852b7871b516e7271e11661349e35c5926f523
Gilberto <giba.darocha@gmail.com>
2026-02-09 07:21:11 -0300

    WIP - Added backend to K3d

--- src/backend/eng/K8s/K3d.sh
+++ src/backend/eng/K8s/K3d.sh
@@ -1,11 +1,16 @@
 # create a new cluster
-k3d cluster create mentory -p "8081:80@loadbalancer" --agents 2
+k3d registry create mentoryregistry.localhost --port 5000
+k3d cluster create mentory --registry-create mentoryregistry.localhost:5000 --port "5002:5002@loadbalancer"
 
 # configure the kubectl to the new cluster
 k3d kubeconfig merge mentory --kubeconfig-switch-context
 
 # add backend webapi to cluster
 k3d image import hotline-webapi:latest -c mentory
+
+# push das imagens
+docker tag hotline-webapi:latest localhost:5000/hotline-webapi:latest
+docker push localhost:5000/hotline-webapi:latest
 
 
 # apply the deploy and service
@@ -15,3 +20,5 @@ kubectl apply -f service.yml
 
 # list of services
 kubectl get svc
+
+
